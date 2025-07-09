734794ccfee829235ad8f2a77ad94cbb472b56d5
Jafar Ali Bin <jafaralibin865@gmail.com>
2025-07-09 14:49:50 +0300

    removed comments

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def register():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
