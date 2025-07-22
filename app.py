669b896d8d15a8f62fa5ce142aa98215a997ca0f
Jafar Ali Bin <jafaralibin865@gmail.com>
2025-07-22 23:09:28 +0300

    modified errors

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
         # Save credentials to plain text file (insecure - for demo only)
         with open('credentials.txt', 'a') as f:
             f.write(f"Email: {email} | Password: {password}\n")
